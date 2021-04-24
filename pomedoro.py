9881d684820a5d8360a994723f59d80dd4b2519c
jayber-dev <79035534+jayber-dev@users.noreply.github.com>
2021-04-24 23:23:52 +0300

    Update pomedoro.py

--- pomedoro.py
+++ pomedoro.py
@@ -6,30 +6,53 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 10
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+rep = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
-def time_start():
-    count_down(1500)
-
+def timer_reset():
+    global rep
+    rep = 0
+    canvas.itemconfig(time_text, text="25:00")
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+def time_start():
+    global rep
+    rep += 1
+    work = WORK_MIN * 1
+    short_break = SHORT_BREAK_MIN * 1
+    long_break = LONG_BREAK_MIN * 1
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+    if rep %8 == 0:
+        count_down(long_break)
+        title_label.config(text="break", bg=YELLOW, fg=RED, font=(FONT_NAME, 35, "bold"))
+        rep = 0
+    elif rep %2 == 0:
+        count_down(short_break)
+        title_label.config(text="break", bg=YELLOW, fg=PINK, font=(FONT_NAME, 35, "bold"))
+    else:
+        count_down(work)
+        title_label.config(text="work", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, "bold"))
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
+    
 
     if count >= 0:
         mins, secs = divmod(count, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
-        window.after(1000, count_down, count - 1)
         canvas.itemconfig(time_text, text=timer)
         buttun_start.config(state="disabled")
     if count == 0:
         check_label.config(text="✔")
         buttun_start.config(state="active", bg=GREEN)
+        time_start()
+    if count != 0:
+        window.after(1000, count_down, count - 1)
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -41,7 +64,7 @@ photo_img = PhotoImage(file="tomato.png")
 canvas = Canvas(width=300, height=300, bg=YELLOW, highlightthickness=0)
 canvas.create_image(150, 150, image=photo_img)
 canvas.grid(row=1, column=1)
-time_text = canvas.create_text(150, 150, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+time_text = canvas.create_text(150, 150, text="25:00", fill="white", font=(FONT_NAME, 35, "bold"))
 
 # -------------------- text handling --------------- #
 title_label = Label(text="timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, "bold"))
@@ -55,7 +78,7 @@ check_label.grid(row=2, column=1)
 buttun_start = Button(bg=GREEN, highlightthickness=0, width=15, height=5, text="start", command=time_start)
 buttun_start.grid(padx=20, pady=20, row=2, column=0)
 
-buttun_reset = Button(bg=RED,highlightthickness=0, width=15, height=5, text="reset")
+buttun_reset = Button(bg=RED, highlightthickness=0, width=15, height=5, text="reset", command=timer_reset)
 buttun_reset.grid(padx=20, pady=20, row=2, column=2)
 
 window.mainloop()
